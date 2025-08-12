ff13431b356eae152aa4f1ad769654d76f9241bc
Rodrigo <rodrigo.markowicz@hexagon.com>
2025-08-12 18:37:16 -0300

    Inicial 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.qq.qqpega"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.qq.qqpega"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -49,29 +49,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.play.services.location)
-    implementation(libs.play.services.maps)
-    implementation(libs.maps)
-    implementation(libs.androidx.navigation.compose.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-        //FireBase
-    implementation (platform(libs.firebase.bom))
-    implementation (libs.firebase.auth)
-    implementation (libs.androidx.navigation.compose)
-  
-    //Maps
-    implementation (libs.accompanist.permissions)
-
-    implementation(libs.maps.compose)
-
-    implementation (libs.volley)
-
-
 }
--- app/src/main/java/com/qq/qqpega/MainActivity.kt
+++ app/src/main/java/com/qq/qqpega/MainActivity.kt
@@ -4,131 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.firebase.FirebaseApp
-import com.qq.qqpega.login.LoginScreen
-import com.qq.qqpega.map.CategoriasSection
-import com.qq.qqpega.map.DestaquesSection
-import com.qq.qqpega.map.MapScreen
-import android.Manifest
+import com.qq.qqpega.ui.theme.QQpegaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this)
-
+        enableEdgeToEdge()
         setContent {
-            /*MaterialTheme {
-                val navController = rememberNavController()
-
-                NavHost(navController = navController, startDestination = "login") {
-                    composable("login") {
-                        LoginScreen {
-                            navController.navigate("map") {
-                                popUpTo("login") { inclusive = true }
-                            }
-                        }
-                    }
-                    composable("map") {
-                        MapScreen()
-                    }
+            QQpegaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
-            }*/
-            //MapScreen()
-            SolicitarPermissaoLocalizacao {
-                MapScreen()
             }
-            //MapScreen()
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF1B1534))
-                    .padding(16.dp)
-            ) {
-
-                CategoriasSection()
-                DestaquesSection()
-                MapScreen()
-
-            }
-            //CategoriasSection()
         }
     }
 }
-@OptIn(ExperimentalPermissionsApi::class)
+
 @Composable
-fun SolicitarPermissaoLocalizacao(onPermissaoConcedida: @Composable () -> Unit) {
-    val permissoes = rememberMultiplePermissionsState(
-        listOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
     )
-
-    LaunchedEffect(Unit) {
-        permissoes.launchMultiplePermissionRequest()
-    }
-
-    if (permissoes.allPermissionsGranted) {
-        onPermissaoConcedida()
-    } else {
-        // Apenas exibe uma mensagem caso o usuário ainda não aceitou
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("Permissão de localização necessária")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = { permissoes.launchMultiplePermissionRequest() }) {
-                    Text("Permitir")
-                }
-            }
-        }
-    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF1B1534))
-            .padding(16.dp)
-    ) {
-
-        CategoriasSection()
-        DestaquesSection()
-
-        MapScreen()
-
+    QQpegaTheme {
+        Greeting("Android")
     }
 }
